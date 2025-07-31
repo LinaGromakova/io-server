5f09e14b7312b0bbb58061c8e9c81260666d317c
Lina Nevan <linaWebDev@outlook.com>
2025-07-31 21:14:31 +0300

    Add session

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const app = express();
 const route = require('./route');
+const session = require('express-session');
+const PgSession = require('connect-pg-simple')(session);
+const pool = require('./db');
 
+app.use(
+  session({
+    store: new PgSession({ pool }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 app.use(express.json());
 app.use(cors({ origin: '*' }));
 app.use(route);
--- route.js
+++ route.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const { users } = require('./users');
 const router = express.Router();
 const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
 
 const db = require('./db').default;
 
+function authMiddleware(req, res, next) {
+  if (req.session && req.session.authenticated) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
 router.post('/register', async (req, res) => {
   try {
-    const { login, password } = req.body;
+    const { login, name, password } = req.body;
 
     const existingUser = await db.query('SELECT * FROM users WHERE login=$1', [
       login,
@@ -19,7 +26,6 @@ router.post('/register', async (req, res) => {
     }
 
     const id = nanoid();
-    const name = 'Ванечка';
     const passwordHash = await bcrypt.hash(password, 10);
 
     const newPerson = await db.query(
@@ -39,11 +45,12 @@ router.post('/register', async (req, res) => {
   }
 });
 
-router.get('/', async (_req, res) => {
+router.get('/', authMiddleware, async (_req, res) => {
   const users = await db.query('SELECT * FROM users');
+  console.log(users);
   res.send(users.rows[0]);
 });
-router.get('/:id', async (req, res) => {
+router.get('/:id', authMiddleware, async (req, res) => {
   const userId = req.params.id;
   const currentUser = await db.query('SELECT * FROM users where id = $1', [
     userId,
@@ -69,8 +76,14 @@ router.post('/login', async (req, res) => {
     if (!validPassword) {
       return res.status(401).json({ message: 'Неверный логин или пароль' });
     }
-    console.log(validPassword);
-    return res.status(200).json({ message: `Мы скучали, ${currentUser.name}` });
+
+    req.session.user_id = currentUser.id;
+    req.session.authenticated = true;
+    console.log(req.session.authenticated);
+    return res.status(200).json({
+      message: `Мы скучали, ${currentUser.name}`,
+      auth: req.session.authenticated,
+    });
   } catch (error) {
     console.error('Ошибка авторизации:', error);
     return res
@@ -78,4 +91,5 @@ router.post('/login', async (req, res) => {
       .json({ message: 'Что-то пошло не так. Пожалуйста, попробуйте снова.' });
   }
 });
+
 module.exports = router;
